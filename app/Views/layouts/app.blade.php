4eebebe6fd88f638cf66e4913ab13f0dfe0e6ce0
srustamov <rustemovv96@gmail.com>
2019-08-14 14:04:29 +0400

    some fix

--- app/Views/layouts/app.blade.php
+++ app/Views/layouts/app.blade.php
@@ -12,7 +12,7 @@
   </head>
   <body>
       <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-lg">
-          <a class="navbar-brand" href="#">
+          <a class="navbar-brand" href="/">
               <strong class="text-primary">{{config('app.name','TT')}}</strong>
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
