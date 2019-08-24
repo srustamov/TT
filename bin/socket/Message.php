7333ca13504a8c11391bc9dd0a413a3c931ac9cd
Samir Rustamov <rustemovv96@gmail.com>
2019-08-24 10:37:00 +0400

    some fix

--- bin/socket/Message.php
+++ bin/socket/Message.php
@@ -5,7 +5,7 @@ use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
 
-require dirname(dirname(__DIR__)).'/vendor/autoload.php';
+require dirname(__DIR__,2).'/vendor/autoload.php';
 
 
 class Message implements MessageComponentInterface
